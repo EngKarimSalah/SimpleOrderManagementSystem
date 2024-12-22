b6494551ad5168c7d17330a53e12ddab6a1fe348
karim <karim@gmail.com>
2024-12-22 09:48:30 +0400

    Initiate the database ( adding models and setup connection )

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleOrderManagementSystem.Models;
+
+namespace SimpleOrderManagementSystem
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+    }
+}
